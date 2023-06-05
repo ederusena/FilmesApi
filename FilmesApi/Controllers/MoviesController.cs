72e1cd2ea369fe2746c0b51824915c326d43a794
ederusena <eder.sena@live.com>
2023-06-04 23:24:40 -0300

    Add project files.

---
+++ FilmesApi/Controllers/MoviesController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using FilmesApi.Models;
+
+namespace FilmesApi.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class MoviesController : Controller
+{
+   private static List<Movie> movies = new List<Movie>();
+
+    [HttpPost]
+    public void AddMovie([FromBody] Movie movie)
+    {
+        movies.Add(movie);
+    }
+
+
+}
---
+++ FilmesApi/Models/Movie.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FilmesApi.Models;
+
+public class Movie
+{
+    public int Id { get; set; }
+
+    [Required(ErrorMessage = "O titulo do filme é obrigatorio.")]
+    public string? Title { get; set; }
+    [Required(ErrorMessage = "O genero do filme é obrigatorio.")]
+    [MaxLength(50, ErrorMessage = "Maximo de 50 caracteres para Genero.")]
+    public string? Genre { get; set; }
+    [Required(ErrorMessage = "O duraçãoW do filme é obrigatorio.")]
+    [Range(70, 600, ErrorMessage = "A duração deve ter entre 70 e 600 minutos")]
+    public int Duration { get; set; }
+    
+}
